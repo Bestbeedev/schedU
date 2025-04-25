9046245a9937c1c3da06648b62c5350627ff9827
Bestbeedev <josueaoga0@gmail.com>
2025-04-25 01:58:25 +0100

    setup types pros for buiding

--- components/layouts/GradientWrapper.tsx
+++ components/layouts/GradientWrapper.tsx
@@ -1,18 +1,26 @@
-export default function GradientWrapper ({ children, ...props }:any) {
-return(
-    <div
-        {...props}
-        className={`relative ${props.className || ""}`}>
-        <div className={`absolute m-auto blur-[160px] ${props.wrapperclassname || ""}`}
-            style={{
-                background:
-                    "linear-gradient(180deg, #7C3AED 0%, rgba(152, 103, 240, 0.984375) 0.01%, rgba(237, 78, 80, 0.2) 100%)",
-            }}>
+import React from "react";
 
-        </div>
-        <div className="relative">
-            {children}
-        </div>
+interface GradientWrapperProps extends React.HTMLAttributes<HTMLDivElement> {
+  wrapperclassname?: string;
+  children: React.ReactNode;
+}
+
+export default function GradientWrapper({
+  children,
+  wrapperclassname = "",
+  className = "",
+  ...props
+}: GradientWrapperProps) {
+  return (
+    <div {...props} className={`relative ${className}`}>
+      <div
+        className={`absolute m-auto blur-[160px] ${wrapperclassname}`}
+        style={{
+          background:
+            "linear-gradient(180deg, #7C3AED 0%, rgba(152, 103, 240, 0.984375) 0.01%, rgba(237, 78, 80, 0.2) 100%)",
+        }}
+      />
+      <div className="relative">{children}</div>
     </div>
-)}
-
+  );
+}
--- components/layouts/LayoutEffect.tsx
+++ components/layouts/LayoutEffect.tsx
@@ -1,20 +1,29 @@
-"use client"
+'use client';
 
-import { useInView } from "framer-motion";
-import { useRef } from "react";
+import { useInView } from 'framer-motion';
+import { useRef, ReactNode } from 'react';
+
+interface LayoutEffectProps {
+  children: ReactNode;
+  className?: string;
+  isInviewState: {
+    trueState?: string;
+    falseState?: string;
+  };
+}
 
 export default function LayoutEffect({
   children,
-  className,
-  isInviewState: { trueState = "", falseState = "" },
-}: any) {
+  className = '',
+  isInviewState: { trueState = '', falseState = '' },
+}: LayoutEffectProps) {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
   return (
     <div
       ref={ref}
-      className={`${className || ""} ${isInView ? trueState : falseState}`}
+      className={`${className} ${isInView ? trueState : falseState}`}
     >
       {children}
     </div>
