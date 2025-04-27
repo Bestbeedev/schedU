504146e9e2e921fe48867f0ee7cc046c8694d403
Bestbeedev <josueaoga0@gmail.com>
2025-04-27 11:46:54 +0100

    Setup config for pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,21 @@
-
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { AuthInitializer } from "@/lib/authInitializer";
 import { Toaster } from "sonner";
 import NetworkStatusListener from "@/components/layouts/NetworkStatus";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "schedU - Votre planning étudiant",
+  description: "Consultez facilement votre programme de cours hebdomadaire avec schedU.",
+  manifest: "/manifest.json",
+  themeColor: "#16a34a",
+  icons: {
+    icon: "/icons/icon192x192.png",
+    apple: "/icons/icon192x192.png", // pour iOS
+  },
+};
 
 export default function RootLayout({
   children,
@@ -11,7 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="fr" suppressHydrationWarning>
+      <head>
+        {/* fallback pour certaines balises pas encore couvertes par metadata */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#16a34a" />
+        <link rel="apple-touch-icon" href="/icons/icon192x192.png" />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
@@ -20,11 +38,12 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <AuthInitializer />
-          <NetworkStatusListener/>
+          <NetworkStatusListener />
           <Toaster 
-          closeButton 
-          theme="light"
-          position="top-center"/>
+            closeButton 
+            theme="light"
+            position="top-center"
+          />
           {children}
         </ThemeProvider>
       </body>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   /* config options here */
 };
 
+const withPWA = require('next-pwa')({
+  dest: 'public', // où seront générés les fichiers du service worker
+  disable: process.env.NODE_ENV === 'development', // active PWA seulement en production
+})
+
+module.exports = withPWA(nextConfig)
 export default nextConfig;
+
+
