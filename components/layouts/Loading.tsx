3fe1f81e9797b93acf998ef9be14529a804da800
Bestbeedev <josueaoga0@gmail.com>
2025-04-27 12:29:50 +0100

    max-sm margin for mobile Card loading ,signup and login

--- components/layouts/Loading.tsx
+++ components/layouts/Loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="flex items-center w-screen dark:bg-neutral-800 justify-center bg-white min-h-screen">
-      <div className="text-center dark:bg-neutral-700/40 mx-auto p-6 h-fit border bg-neutral-50 max-w-sm rounded-lg w-fit">
+      <div className="text-center dark:bg-neutral-700/40 mx-auto p-6 h-fit border bg-neutral-50 max-w-sm max-sm:mx-4 rounded-lg w-fit">
       <div className=" flex mx-auto text-center  size-10 my-4 animate-spin rounded-full border-2 border-green-500 border-t-transparent" />
         <p className="text-lg">Veuillez patienter, chargement des données...</p>
       </div>
